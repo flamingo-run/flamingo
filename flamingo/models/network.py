ad0df711bf47b4177b9680704e6e9e36487e140d
Joao Daher <joao@daher.dev>
2021-03-12 18:37:47 -0300

    Add support to zone name

--- flamingo/models/network.py
+++ flamingo/models/network.py
@@ -8,5 +8,13 @@ from models.project import Project
 @dataclass
 class Network(EmbeddedDocument):
     zone: str
+    zone_name: str = None
     project: Project = field(default_factory=Project.default_for_network)
     vpc_connector: str = None
+
+    def __post_init__(self):
+        if not self.zone_name:
+            self.zone_name = self.zone.strip('.').replace('.', '-')
+
+    def get_record_name(self, domain):
+        return f'{domain}.{self.zone}'
