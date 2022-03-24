5e1ead00b0c6f67d5a578f3b88aa567c5f5a108d
João Daher <joao@daher.dev>
2022-03-24 14:35:43 -0300

    feat: add support to service account keys

--- flamingo/models/service_account.py
+++ flamingo/models/service_account.py
@@ -1,7 +1,10 @@
+import base64
+import json
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Dict, ClassVar
 
 from gcp_pilot.datastore import EmbeddedDocument
+from gcp_pilot.iam import IdentityAccessManager
 
 from models.project import Project
 
@@ -13,6 +16,9 @@ class ServiceAccount(EmbeddedDocument):
     display_name: str = ""
     roles: List[str] = field(default_factory=list)
     project: Project = field(default_factory=Project.default)
+    key: str = None
+
+    _exclude_from_indexes: ClassVar = ("key",)
 
     @property
     def email(self) -> str:
@@ -20,3 +26,15 @@ class ServiceAccount(EmbeddedDocument):
 
     def get_all_roles(self):
         return self.roles + ['run.admin']
+
+    def rotate_key(self):
+        iam = IdentityAccessManager(project_id=self.project.id)
+        for key_data in iam.list_keys(service_account_name=self.name):
+            if key_data["keyType"] == "USER_MANAGED":
+                iam.delete_key(id=key_data["id"], service_account_name=self.name)
+        key_data = iam.create_key(service_account_name=self.name)
+        self.key = key_data["privateKeyData"]
+
+    @property
+    def json_key(self) -> str:
+        return base64.b64decode(self.key).decode()
