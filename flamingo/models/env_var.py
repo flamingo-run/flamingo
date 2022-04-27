84664eeb0214941b90f8608ab0a41661dfcf2ebe
João Daher <joao@daher.dev>
2022-04-27 17:14:08 -0300

    fix: upgrade gcp-pilot and sanic-rest

--- flamingo/models/env_var.py
+++ flamingo/models/env_var.py
@@ -16,12 +16,6 @@ class EnvVar(KeyValueEmbeddedDocument):
     is_secret: bool = False
     source: EnvVarSource = EnvVarSource.USER
 
-    def to_dict(self) -> Dict:
-        data = super().to_dict()
-        # if self.is_secret:
-        #     data['value'] = REDACTED
-        return data
-
     @property
     def is_implicit(self):
         return self.source == EnvVarSource.FLAMINGO
