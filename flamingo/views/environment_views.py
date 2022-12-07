4fc4ddc16f21a7eb80aa8a9265a06918b4ae9018
Guilherme Araújo <guilhermeama@gmail.com>
2022-12-07 13:17:44 -0300

    feat: add route to clone environment

--- flamingo/views/environment_views.py
+++ flamingo/views/environment_views.py
@@ -4,6 +4,7 @@ from sanic_rest import exceptions
 from sanic_rest.views import DetailView, ListView, NestedListView, ResponseType
 
 from models.environment import Environment
+from services.clone import EnvironmentClone
 from services.foundations import EnvironmentFoundation
 
 environments = Blueprint("environments", url_prefix="/environments")
@@ -15,6 +16,28 @@ class EnvironmentListView(ListView):
 
 class EnvironmentDetailView(DetailView):
     model = Environment
+
+
+class EnvironmentCloneView(NestedListView):
+    nest_model = Environment
+
+    async def perform_get(self, request: Request, nest_obj: Environment) -> ResponseType:
+        raise exceptions.NotAllowedError()
+
+    async def perform_post(self, request: Request, nest_obj: Environment) -> ResponseType:
+        data, _ = self._parse_body(request=request)
+        app = EnvironmentClone(environment=nest_obj).clone(
+            env_name=data.get("environment_name"),
+            project_id=data.get("project_id"),
+        )
+
+        return app.to_dict(), 201
+
+    async def perform_put(self, request: Request, nest_obj: Environment) -> ResponseType:
+        raise exceptions.NotAllowedError()
+
+    async def perform_delete(self, request: Request, nest_obj: Environment) -> ResponseType:
+        raise exceptions.NotAllowedError()
 
 
 class EnvironmentInitializeView(NestedListView):
@@ -39,4 +62,5 @@ class EnvironmentInitializeView(NestedListView):
 
 environments.add_route(EnvironmentListView.as_view(), "/")
 environments.add_route(EnvironmentDetailView.as_view(), "/<pk>")
+environments.add_route(EnvironmentCloneView.as_view(), "/<nest_pk>/clone")
 environments.add_route(EnvironmentInitializeView.as_view(), "/<pk>/init")
